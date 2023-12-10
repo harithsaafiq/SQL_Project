88b7011708335d9877f0d4d8fad8612ecc454488
Harithsa Afiq <139738179+harithsaafiq@users.noreply.github.com>
2023-12-10 17:52:50 +0800

    Add files via upload

---
+++ Portfolio_Project_1.sql
@@ -0,0 +1,116 @@
+/*
+Data Exploration
+*/
+
+SELECT *
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null
+ORDER BY 3,4
+
+--SELECT *
+--FROM PortfolioProject..CovidVaccinations
+--ORDER BY 3,4
+
+--Select Data that we are going to be using
+
+SELECT Location, date, total_cases, new_cases, total_deaths, population
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null
+ORDER BY 1,2
+
+-- Looking at Total Cases vs Total Deaths
+-- Shows likehood of dying if you contract covid in your country
+
+SELECT Location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
+FROM PortfolioProject..CovidDeaths
+WHERE location like '%malaysia%' and continent is not null
+ORDER BY 1,2 DESC
+
+--Looking at Total Cases vs Population
+--Shows what percentage of population of got covid
+
+SELECT Location, date, population, total_cases, (total_cases/population)*100 AS PercentPopulationInfected
+FROM PortfolioProject..CovidDeaths
+WHERE location like '%malaysia%'
+ORDER BY 1,2 DESC
+
+--Looking at Countries with Highest Infection Rate compared to Population
+
+SELECT Location, population, MAX(total_cases) AS HighestInfectionCount, MAX((total_cases/population)*100) AS PercentPopulationInfected
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null and population is not null
+GROUP BY Location, population
+ORDER BY 4 DESC
+
+-- Show Countries with Highest Death Count per Population
+
+SELECT Location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null and total_deaths is not null
+GROUP BY Location
+ORDER BY 2 DESC
+
+-- Show Continents with Highest Death Count per Population - 1
+
+SELECT continent, MAX(cast(Total_deaths as int)) as TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+WHERE continent is not null and total_deaths is not null
+GROUP BY continent
+ORDER BY 2 DESC
+
+ --Showing Continents with Highest Death Count per Population - 2
+
+SELECT location, MAX(cast(Total_deaths as int)) as TotalDeathCount
+FROM PortfolioProject..CovidDeaths
+WHERE continent is null and total_deaths is not null
+GROUP BY location
+ORDER BY 2 DESC
+
+--Global Numbers
+
+SELECT date, SUM(new_cases) AS NewCasesCounts, SUM(cast(new_deaths as int)) AS DeathCounts, (SUM(cast(new_deaths as int))/SUM(new_cases))*100 AS DeathPercentage
+FROM PortfolioProject..CovidDeaths
+--WHERE location like '%malaysia%' and continent is not null
+WHERE continent is not null
+GROUP BY date
+ORDER BY 1,2
+
+SELECT SUM(new_cases) AS NewCasesCounts, SUM(cast(new_deaths as int)) AS DeathCounts, (SUM(cast(new_deaths as int))/SUM(new_cases))*100 AS DeathPercentage
+FROM PortfolioProject..CovidDeaths
+--WHERE location like '%malaysia%' and continent is not null
+WHERE continent is not null
+--GROUP BY date
+ORDER BY 1,2
+
+--COVID VACCINATIONS
+
+--Looking at Total Population vs Vaccinations
+
+SELECT dea.continent, dea.location, dea.date, population, vac.new_vaccinations, 
+SUM(CONVERT(int, vac.new_vaccinations)) OVER (Partition by dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date
+WHERE dea.continent is not null
+ORDER BY 2,3
+
+--USE CTE (Common Table Expressions)
+
+WITH PopVsVac (Continent, Location, Date, Population, new_vaccinations, RollingPeopleVaccinated)
+AS
+(
+SELECT dea.continent, dea.location, dea.date, population, vac.new_vaccinations 
+, SUM(CONVERT(int, vac.new_vaccinations)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date) AS RollingPeopleVaccinated
+
+FROM PortfolioProject..CovidDeaths dea
+JOIN PortfolioProject..CovidVaccinations vac
+	ON dea.location = vac.location
+	and dea.date = vac.date
+WHERE dea.continent is not null
+)
+SELECT *, (RollingPeopleVaccinated/Population)*100 AS PercentPopulationVaccinated
+FROM PopVsVac
+WHERE RollingPeopleVaccinated is not null
+
+--TEMP TABLE
